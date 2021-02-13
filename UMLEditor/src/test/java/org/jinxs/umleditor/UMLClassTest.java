be98a8209c2dee93dc5215fe69c0e126790a7896
Jonathan Wilkins <jonathan.wilkins28@gmail.com>
2021-02-13 18:44:40 -0500

    Fixed maven files and pom.xml so packaging and testing now works

---
+++ UMLEditor/src/test/java/org/jinxs/umleditor/UMLClassTest.java
@@ -0,0 +1,18 @@
+package org.jinxs.umleditor;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/*
+ * Unit test for UMLClass.
+ */
+public class UMLClassTest  {
+    
+    @Test
+    public void firstTest()
+    {
+
+    }
+
+}
