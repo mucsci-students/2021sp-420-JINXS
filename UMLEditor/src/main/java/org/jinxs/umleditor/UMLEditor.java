df2e7fb39d74180b8745b7ceb8aeca143272eaa7
Irblake14 <Ian.blake191@gmail.com>
2021-03-01 17:40:22 -0800

    Added Additional GUI methods and fixed some errors in UMLEditor

--- UMLEditor/src/main/java/org/jinxs/umleditor/UMLEditor.java
+++ UMLEditor/src/main/java/org/jinxs/umleditor/UMLEditor.java
@@ -105,8 +105,8 @@ public class UMLEditor {
             return;
         }
         /*******************Place holder type check*******************/
-        if (type.equals("association") || type.equals("aggregation") || type.equals("composition") ||
-            type.equals("generalization")){
+        if (type.equals("inheritance") || type.equals("aggregation") || type.equals("composition") ||
+            type.equals("realization")){
 
         }else if (!(type.equals(""))){
             System.out.println("Missing a Type, Please try again");
@@ -215,8 +215,8 @@ public class UMLEditor {
         UMLClass c2 = classExists(class2);
 
         /******************* Place holder type check *******************/
-        if (newType.equals("association") || newType.equals("aggregation") || newType.equals("composition")
-                || newType.equals("generalization")) {
+        if (newType.equals("inheritance") || newType.equals("aggregation") || newType.equals("composition")
+                || newType.equals("realization")) {
 
         } else if (!(newType.equals(""))) {
             System.out.println("Missing a Type, Please try again");
@@ -461,6 +461,11 @@ public class UMLEditor {
         for (int i = 0; i < classes.size(); ++i) {
             System.out.println(classes.get(i).name);
         }
+    }
+
+    public ArrayList<UMLClass> getClasses(){
+
+        return classes; 
     }
 
     public void save(String fileName) {
